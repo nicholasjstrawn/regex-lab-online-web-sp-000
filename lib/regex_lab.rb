6ef583827f56fdf4f0f27922c2927a9a7d47d316
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-24 22:22:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.scan(/^[aeiouAEIOU]/)
+  if word.match(/\A[aeiouAEIOU]/) == nil
+    return false
+  else
+    return true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
