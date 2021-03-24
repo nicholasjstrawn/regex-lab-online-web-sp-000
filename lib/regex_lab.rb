ea38d583a9250bec6a8b19faf548ee8f68658ff4
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-03-24 22:23:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiouAEIOU]/) == nil
-    return false
-  else
+  if word.match(/\A[aeiouAEIOU]/) == true
     return true
+  else
+    return false
   end
 end
 
